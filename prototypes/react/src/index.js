75fda08739beead2766e6de545ba4614c139d815
Andrew-CC-Martin <amartin262@gmail.com>
2020-05-01 09:41:50 +1000

    stashing changes

--- prototypes/react/src/index.js
+++ prototypes/react/src/index.js
@@ -16,6 +16,9 @@ class App extends React.Component {
 
   stagePieces = []
 
+  snapDistance = 20
+  // layer = []
+
   buildPieces = () => {
     const { image, canvas } = this.refs
     const { rowLength, columnLength } = this.state
@@ -33,6 +36,7 @@ class App extends React.Component {
     })
 
     const layer = new Konva.Layer()
+    this.layer = layer
 
     boardData.forEach((row, i) => {
       this.stagePieces.push([])
@@ -44,19 +48,31 @@ class App extends React.Component {
         const c = cvs.getContext('2d')
         c.putImageData(imageData, 0, 0)
 
+        const group = new Konva.Group({
+          draggable: true
+        })
+
         const img = new Konva.Image({
           image: cvs,
-          x: boardData[i][j].x,
-          y: boardData[i][j].y,
+          // x: boardData[i][j].x,
+          // y: boardData[i][j].y,
+          x: 0,
+          y: 0,
           width: boardData[i][j].width,
           height: boardData[i][j].height,
-          draggable: true,
+          // draggable: true,
+          draggable: false,
           row: i,
-          col: j
+          col: j,
+          joined: []
         })
-        this.stagePieces[i].push(img)
 
-        layer.add(img)
+        layer.add(group)
+        group.add(img)
+        // console.log('group after add image:', group)
+        this.stagePieces[i].push(img)
+        // this.stagePieces[i].push(group)
+        // layer.add(group)
       })
     })
 
@@ -64,35 +80,76 @@ class App extends React.Component {
     stage.add(layer)
   }
 
-  handleDragEndStage = (target) => {
-    this.maybeSnapToPartner(target)
+  handleDragEndStage = (group) => {
+    // iterate through each child node
+    // if it meets a partner, snap (group)
+    group.children.forEach(piece => {
+      this.maybeSnapToPartner(piece)
+    })
+  }
+
+  isJoined = (joined, partner) => {
+    if (joined.length === 0) {
+      return false
+    }
+    
+    let returnValue = false
+    joined.forEach((joinedPiece) => {
+      if (joinedPiece.row === partner.row && joinedPiece.col === partner.col) {
+        returnValue = true
+      }
+    })
+
+    return returnValue
   }
 
   /**
    * Check if the piece is close to one of its partner pieces
    * If it is, snap into place
    */
-  maybeSnapToPartner = (target) => {
+  maybeSnapToPartner = (piece) => {
     const { rowLength, columnLength, pieceWidth, pieceHeight } = this.state
-    const { attrs: { row, col } } = target
-
-    // distance where pieces will snap into place
-    const distance = pieceHeight / 10
+    const { attrs: { row, col } } = piece 
 
     // find out who partners are
     const abovePartner = row === 0 ? null : { row: row - 1, col }
-    if (abovePartner) {
+    if (!!abovePartner && !this.isJoined(piece.attrs.joined, abovePartner)) {
       const abovePiece = this.stagePieces[abovePartner.row][abovePartner.col]
-      const targetX = abovePiece.attrs.x
-      const targetY = abovePiece.attrs.y + pieceHeight
-      if ((Math.abs(targetX - target.attrs.x) < distance) && (Math.abs(targetY - target.attrs.y) < distance)) {
-        target.to({
-          x: targetX,
-          y: targetY,
-          duration: 0.5,
-          easing: Konva.Easings.ElasticEaseOut
-        })
+
+      const targetTrueX = abovePiece.parent.attrs.x + abovePiece.attrs.x
+      const targetTrueY = abovePiece.parent.attrs.y + abovePiece.attrs.y + pieceHeight
+      const pieceTrueX = piece.parent.attrs.x + piece.attrs.x
+      const pieceTrueY = piece.parent.attrs.y + piece.attrs.y
+
+
+      if ((Math.abs(targetTrueX - pieceTrueX) < this.snapDistance) &&
+        (Math.abs(targetTrueY - pieceTrueY) < this.snapDistance)) {
+          piece.attrs.joined.push(abovePartner)
+          piece.parent.children.forEach((piece) => {
+            piece.setAttrs({
+              x: piece.attrs.x + abovePiece.attrs.x,
+              y: piece.attrs.y + abovePiece.attrs.y + pieceHeight
+            })
+            // add each piece to the group of the above piece
+            // abovePiece.parent.add(piece)
+            piece.moveTo(abovePiece.parent)
+            })
+          // kill the piece's original group (don't need it anymore)
+          // piece.parent.destroy()
+          // console.log("App -> piece.parent", piece.parent)
+
+          const selectionRect = new Konva.Rect({
+            stroke: 'blue',
+            // visible: true,
+            listening: false
+          })
+          this.layer.add(selectionRect)
+          selectionRect.setAttrs(piece.getParent().getClientRect())
+
+          this.layer.draw()
       }
+    } else {
+      // console.log('doesn\'t have an unjoined above partner')
     }
 
     const belowPartner = row + 1 >= rowLength ? null : { row: row + 1, col }
@@ -100,8 +157,8 @@ class App extends React.Component {
       const belowPiece = this.stagePieces[belowPartner.row][belowPartner.col]
       const targetX = belowPiece.attrs.x
       const targetY = belowPiece.attrs.y - pieceHeight
-      if ((Math.abs(targetX - target.attrs.x) < distance) && (Math.abs(targetY - target.attrs.y) < distance)) {
-        target.to({
+      if ((Math.abs(targetX - piece.attrs.x) < this.snapDistance) && (Math.abs(targetY - piece.attrs.y) < this.snapDistance)) {
+        piece.to({
           x: targetX,
           y: targetY,
           duration: 0.5,
@@ -115,9 +172,9 @@ class App extends React.Component {
       const leftPiece = this.stagePieces[leftPartner.row][leftPartner.col]
       const targetX = leftPiece.attrs.x + pieceWidth
       const targetY = leftPiece.attrs.y
-      if ((Math.abs(targetX - target.attrs.x) < distance) && (Math.abs(targetY - target.attrs.y) < distance)) {
+      if ((Math.abs(targetX - piece.attrs.x) < this.snapDistance) && (Math.abs(targetY - piece.attrs.y) < this.snapDistance)) {
         // piece.setCoordinates(targetX, targetY)
-        target.to({
+        piece.to({
           x: targetX,
           y: targetY,
           duration: 0.5,
@@ -131,8 +188,8 @@ class App extends React.Component {
       const rightPiece = this.stagePieces[rightPartner.row][rightPartner.col]
       const targetX = rightPiece.attrs.x - pieceWidth
       const targetY = rightPiece.attrs.y
-      if ((Math.abs(targetX - target.attrs.x) < distance) && (Math.abs(targetY - target.attrs.y) < distance)) {
-        target.to({
+      if ((Math.abs(targetX - piece.attrs.x) < this.snapDistance) && (Math.abs(targetY - piece.attrs.y) < this.snapDistance)) {
+        piece.to({
           x: targetX,
           y: targetY,
           duration: 0.5,
